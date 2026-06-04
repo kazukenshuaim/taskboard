8eac5794e5065fb07099ff52df46e959bd00ca11
kazukenshuaim <kazu.kenshu.aim@gmail.com>
2026-06-04 09:25:10 +0900

    feat:AbstractUserを継承したCustomUserモデルを追加。accountアプリを作成し、makemigration accountsとmigrate完了

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'tasks',
 ]
 
@@ -128,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# accountsアプリのCustomUserモデルを使用するための設定
+AUTH_USER_MODEL = 'accounts.CustomUser'
