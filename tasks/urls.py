3e81e83c7f4705c7347ad7420972422def072def
kazukenshuaim <kazu.kenshu.aim@gmail.com>
2026-06-05 15:41:45 +0900

    feat: タスクCRUDのうちCreateView、UpdateView、DeleteView、とステータス変更機能を実装。

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView
+from .views import (
+    TaskListView, TaskDetailView,
+    TaskCreateView, TaskUpdateView, TaskDeleteView,
+    TaskStatusUpdateView,
+)
 
 app_name = 'tasks'
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
     path('<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('create/', TaskCreateView.as_view(), name='task_create'),
+    path('<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    path('<int:pk>/status/', TaskStatusUpdateView.as_view(), name='task_status_update'),
 ]
