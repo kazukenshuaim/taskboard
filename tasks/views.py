a008709151898928d1621f2019bf1aebbf57ca9b
kazukenshuaim <kazu.kenshu.aim@gmail.com>
2026-06-05 13:16:20 +0900

    feat: タスク一覧・詳細ビューを実装

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Task
 
 
-class TaskListView(LoginRequiredMixin, TemplateView):
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
     template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        return Task.objects.filter(created_by=self.request.user)
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    context_object_name = 'task'
+
+    def get_queryset(self):
+        # 自分のタスクだけアクセス可能にする（他人のタスク URL に直アクセスすると 404）
+        return Task.objects.filter(created_by=self.request.user)
