f843dea6c88d059f0814759c433d7c6a3d474ac3
kazukenshuaim <kazu.kenshu.aim@gmail.com>
2026-06-11 16:15:47 +0900

    feat: django-debug-toolbar を追加してORM生成SQLを可視化

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/tasks/', permanent=False)),
@@ -24,3 +25,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('tasks/', include('tasks.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
